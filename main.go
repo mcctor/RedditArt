28ceb910bf960f1d2c4346ce94ce30b7ec8ccbc9
mcctor <markvictormwanga@gmail.com>
2019-10-11 11:30:02 +0300

    Refactor code, again :>

--- main.go
+++ main.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"strings"
-
 	"github.com/mcctor/redditart/reddit"
 	"github.com/mcctor/redditart/telegram"
 )
 
 func main() {
-	if !(len(os.Args) > 1) {
-		log.Fatal("Need to provide subreddit names")
-	}
-	for _, subreddit := range os.Args[1:] {
-		go func() {
-			fmtSubredditName := fmt.Sprintf("/r/%s/", strings.ToLower(subreddit))
-			reddit.NewPosts(fmtSubredditName)
-		}()
-	}
+	go reddit.NewPosts("/r/art/")
+	go reddit.NewPosts("/r/artporn/")
+
 	telegram.Bot.Start()
 }
