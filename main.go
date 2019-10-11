20603a1e8d5b63431355c7db79a20ecc05671513
mcctor <markvictormwanga@gmail.com>
2019-10-11 10:30:58 +0300

    Refactor code, again :>

--- main.go
+++ main.go
@@ -5,25 +5,21 @@ import (
 	"log"
 	"os"
 	"strings"
-	"sync"
 
+	_ "github.com/mcctor/redditart/db"
 	"github.com/mcctor/redditart/reddit"
+	"github.com/mcctor/redditart/telegram"
 )
-
-var wg sync.WaitGroup
 
 func main() {
 	if !(len(os.Args) > 1) {
 		log.Fatal("Need to provide subreddit names")
 	}
 	for _, subreddit := range os.Args[1:] {
-		wg.Add(1)
 		go func() {
 			fmtSubredditName := fmt.Sprintf("/r/%s/", strings.ToLower(subreddit))
 			reddit.NewPosts(fmtSubredditName)
-			wg.Done()
 		}()
 	}
-
-	wg.Wait()
+	telegram.Bot.Start()
 }
--- reddit/newart.go
+++ reddit/newart.go
@@ -1,13 +1,19 @@
 package reddit
 
 import (
+	"log"
+	"time"
+
 	"github.com/mcctor/redditart/db"
 	"github.com/mcctor/redditart/reddit/candidates"
 	"github.com/mcctor/redditart/telegram"
+	"github.com/turnage/graw/reddit"
 )
 
 const (
 	historyLimit = 80
+	timeToWait   = 30 // in minutes
+	minutes      = 60
 	minUpVotes   = 100
 )
 
@@ -21,22 +27,45 @@ func isCandidate(upvotes int32) bool {
 
 // NewPosts periodically fetches new posts from given subreddit after stipulated delay
 func NewPosts(subreddit string) {
-	candidatePosts := candidates.GetPosts()
-	harvest, err := redditBot.Listing(subreddit, "")
-	checkError(err)
 
-	for _, post := range harvest.Posts[historyLimit:] {
-		newPost := db.Post{
-			PostID:   post.ID,
-			Caption:  post.Title,
-			Link:     post.Permalink,
-			ImageUrl: post.URL,
-			Author:   post.Author,
+	for {
+		redditBot, err := reddit.NewBot(reddit.BotConfig{
+			Agent: "Ubuntu:github.com/mcctor/goreddit:v0.1.0(by /u/mcctor)",
+			App: reddit.App{
+				ID:       "K_P1LTKylZMqAw",
+				Secret:   "snpKwH-hrKU29KeYFQxQn_wA9aQ",
+				Username: "mcctor",
+				Password: "@lienmwanga01",
+			},
+			Rate: 0,
+		})
+		checkError(err)
+
+		harvest, err := redditBot.Listing(subreddit, "")
+		checkError(err)
+
+		candidatePosts := candidates.GetPosts()
+		for _, post := range harvest.Posts[historyLimit:] {
+			newPost := db.Post{
+				PostID:   post.ID,
+				Caption:  post.Title,
+				Link:     post.Permalink,
+				ImageUrl: post.URL,
+				Author:   post.Author,
+			}
+			if _, exists := candidatePosts.Posts[newPost]; !exists && isCandidate(post.Ups) {
+				candidatePosts.Posts[newPost] = struct{}{}
+				db.AddPost(newPost)
+				telegram.SendPhotoToAll(newPost)
+			}
 		}
-		if _, exists := candidatePosts.Posts[newPost]; !exists && isCandidate(post.Ups) {
-			candidatePosts.Posts[newPost] = struct{}{}
-			db.AddPost(newPost)
-			telegram.SendPhotoToAll(newPost)
-		}
+
+		time.Sleep(timeToWait * minutes)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
 	}
 }
